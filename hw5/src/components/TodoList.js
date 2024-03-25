140a79ffb24b631ab7eb715a9f28334b90a0269d
Yevhenii <evgensokolvak2012@gmail.com>
2024-03-25 19:22:18 +0200

    HW5. Added changes.

--- hw5/src/components/TodoList.js
+++ hw5/src/components/TodoList.js
@@ -14,14 +14,14 @@ export default function TodosList() {
         })();
     }, []);
 
-    const handleMoveToRight = (event) => {
-        if (event.target.dataset.list === "col1") {
-            setTodosCol2((prevState) => [todosCol1[0], ...prevState]);
-            setTodosCol1(todosCol1.slice(1, todosCol1.length));
-        } else {
-            setTodosCol3((prevState) => [todosCol2[0], ...prevState]);
-            setTodosCol2(todosCol2.slice(1, todosCol2.length));
-        }
+    const handleMoveFromFirstToSecond  = () => {
+        setTodosCol2((prevState) => [todosCol1[0], ...prevState]);
+        setTodosCol1(todosCol1.slice(1, todosCol1.length));
+    };
+
+    const handleMoveFromSecondToThird = (event) => {
+        setTodosCol3((prevState) => [todosCol2[0], ...prevState]);
+        setTodosCol2(todosCol2.slice(1, todosCol2.length));
     };
 
     const handleMoveToLeft = (event) => {
@@ -30,8 +30,8 @@ export default function TodosList() {
     };
 
     const handleRemoveLastItem = () => {
-        setTodosCol3((prevState) => prevState.filter((item) => item.id !== todosCol3[todosCol3.length - 1].id));
         service.delete(todosCol3[todosCol3.length - 1].id);
+        setTodosCol3((prevState) => prevState.filter((item) => item.id !== todosCol3[todosCol3.length - 1].id));
     };
 
     return (
@@ -43,7 +43,7 @@ export default function TodosList() {
                     ))}
                 </ul>
                 {todosCol1.length ? 
-                    <button data-list="col1" onClick={(event) => handleMoveToRight(event)}>Transfer first to right</button> 
+                    <button onClick={() => handleMoveFromFirstToSecond()}>Transfer first to right</button> 
                 : null}
             </div>
             <div>
@@ -55,7 +55,7 @@ export default function TodosList() {
                 {todosCol2.length ?
                     <div className="list-btn-wrapper">
                         <button onClick={() => handleMoveToLeft()}>Transfer first to left</button> 
-                        <button onClick={(event) => handleMoveToRight(event)}>Transfer first to right</button> 
+                        <button onClick={() => handleMoveFromSecondToThird()}>Transfer first to right</button> 
                     </div>
                 : null}
             </div>
